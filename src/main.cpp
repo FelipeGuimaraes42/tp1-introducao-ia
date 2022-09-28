99b3e1cc58c13fd0f6429d319c443ae83f2b09da
FelipeGuimaraes42 <felipeguima4@gmail.com>
2022-09-28 19:51:39 -0300

    Starts file input logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "BFS.cpp"
+#include "ReadFile.cpp"
 
-int main() {
-    BFS bfs;
-    std::cout << "Hello, World." << std::endl;
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string fileName(argv[1]);
+    ReadFile readFile(fileName);
+    vector<string> map = readFile.read();
     return 0;
 }
