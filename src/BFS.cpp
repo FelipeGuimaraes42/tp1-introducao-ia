085476e92b768282f6714030394e4d15cf4d2fc5
Felipe Guimaraes <felipeguima4@gmail.com>
2022-10-21 00:47:03 -0300

    Refactor bfs and ids with new previous node logic

--- src/BFS.cpp
+++ src/BFS.cpp
@@ -11,6 +11,7 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
 {
     pair<int, int> initialPoints = {stoi(initialPosition.at(0)), stoi(initialPosition.at(1))};
     pair<int, int> finalPoints = {stoi(initialPosition.at(2)), stoi(initialPosition.at(3))};
+
     if (this->map[finalPoints.first][finalPoints.second] > 6.0 || this->map[initialPoints.first][initialPoints.second] > 6.0)
     {
         cout << "Unreachable. Try another vertice." << endl;
@@ -18,7 +19,6 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
     }
 
     bool isVisited[numLines][numColumns];
-
     for (int i = 0; i < numLines; i++)
     {
         for (int j = 0; j < numColumns; j++)
@@ -36,22 +36,19 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
 
     isVisited[initialPoints.first][initialPoints.second] = true;
 
-    int nodeCounter = 0;
-    int parentCounter = 1;
-
-    Node actualNode(nodeCounter, this->map[initialPoints.first][initialPoints.second], initialPoints, nullptr);
-
-    vector<Node> vertexList;
-    vertexList.push_back(actualNode);
-    nodeCounter++;
-    vertexList.push_back(actualNode);
+    vector<pair<int, int>> headPoints;
+    headPoints.push_back({-1, -1});
+    Node actualNode(this->map[initialPoints.first][initialPoints.second], initialPoints, headPoints, 0);
 
     queue<Node> frontier;
     pair<int, int> adjVertice;
 
     while (true)
     {
-        cout << actualNode.getPoints().first << "," << actualNode.getPoints().second << " ";
+        vector<pair<int, int>> previous = actualNode.getPreviousPoints();
+        previous.push_back(actualNode.getPoints());
+        actualNode.setPreviousPoints(previous);
+
         if (finalPoints.first == actualNode.getPoints().first && finalPoints.second == actualNode.getPoints().second)
         {
             break;
@@ -63,18 +60,18 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
 
             adjVertice = {actualNode.getPoints().first + 1, actualNode.getPoints().second};
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
         if (actualNode.getPoints().first == 0)
@@ -83,9 +80,9 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
         if (actualNode.getPoints().first == numLines - 1)
@@ -94,9 +91,9 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
 
@@ -106,18 +103,18 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
 
             adjVertice = {actualNode.getPoints().first, actualNode.getPoints().second + 1};
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
         if (actualNode.getPoints().second == 0)
@@ -126,9 +123,9 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
         if (actualNode.getPoints().second == numColumns - 1)
@@ -137,43 +134,21 @@ void BFS::getBFS(vector<string> initialPosition, int numLines, int numColumns)
             if (!isVisited[adjVertice.first][adjVertice.second])
             {
                 isVisited[adjVertice.first][adjVertice.second] = true;
-                Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                 frontier.push(node);
-                vertexList.push_back(node);
             }
         }
 
         actualNode = frontier.front();
-        parentCounter = actualNode.getVertex();
         frontier.pop();
     }
-    cout << endl;
 
-    Node *aux;
-    for (int i = vertexList.size() - 1; i >= 0; i--)
+    cout << actualNode.getAccumulatedWeight() << " ";
+    for (int i = 1; i < actualNode.getPreviousPoints().size(); i++)
     {
-        if (vertexList.at(i).getPoints().first == finalPoints.first && vertexList.at(i).getPoints().second == finalPoints.second)
-        {
-            aux = &vertexList.at(i);
-            break;
-        }
-    }
-
-    vector<pair<int, int>> responsePoints;
-    float responseWeight = 0.0;
-    while (aux != nullptr)
-    {
-        responsePoints.push_back(aux->getPoints());
-        responseWeight += aux->getWeight();
-        aux = aux->getPreviousNode();
-    }
-    responseWeight -= this->map[initialPoints.first][initialPoints.second];
-
-    cout << responseWeight << " ";
-
-    for (int i = responsePoints.size() - 1; i >= 0; i--)
-    {
-        cout << "(" << responsePoints.at(i).first << "," << responsePoints.at(i).second << ") ";
+        pair<int, int> aux = actualNode.getPreviousPoints().at(i);
+        cout << "(" << aux.first << "," << aux.second << ") ";
     }
     cout << endl;
 }
--- src/IDS.cpp
+++ src/IDS.cpp
@@ -19,9 +19,9 @@ bool IDS::isValidDepth(pair<int, int> initialPoints, pair<int, int> nextPoint, i
 
 void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
 {
-
     pair<int, int> initialPoints = {stoi(initialPosition.at(0)), stoi(initialPosition.at(1))};
     pair<int, int> finalPoints = {stoi(initialPosition.at(2)), stoi(initialPosition.at(3))};
+
     if (this->map[finalPoints.first][finalPoints.second] > 6.0 || this->map[initialPoints.first][initialPoints.second] > 6.0)
     {
         cout << "Unreachable. Try another vertice." << endl;
@@ -30,13 +30,9 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
 
     bool isVisited[numLines][numColumns];
 
-    int nodeCounter;
-    int parentCounter;
-
-    vector<Node> vertexList;
-
     int depth = 0;
     bool foundSolution = false;
+    Node finalNode;
 
     while (!foundSolution)
     {
@@ -56,25 +52,25 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 }
             }
         }
-        nodeCounter = 0;
-        parentCounter = 1;
+
+        vector<pair<int, int>> headPoints;
+        headPoints.push_back({-1, -1});
+        Node actualNode(this->map[initialPoints.first][initialPoints.second], initialPoints, headPoints, 0);
 
         isVisited[initialPoints.first][initialPoints.second] = true;
-
-        Node actualNode(nodeCounter, this->map[initialPoints.first][initialPoints.second], initialPoints, nullptr);
-        vertexList.clear();
-        vertexList.push_back(actualNode);
-        nodeCounter++;
-        vertexList.push_back(actualNode);
 
         stack<Node> frontier;
         pair<int, int> adjVertice;
 
         while (true)
         {
-            cout << actualNode.getPoints().first << "," << actualNode.getPoints().second << " ";
+            vector<pair<int, int>> previous = actualNode.getPreviousPoints();
+            previous.push_back(actualNode.getPoints());
+            actualNode.setPreviousPoints(previous);
+
             if (finalPoints.first == actualNode.getPoints().first && finalPoints.second == actualNode.getPoints().second)
             {
+                finalNode = actualNode;
                 foundSolution = true;
                 break;
             }
@@ -85,18 +81,18 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
 
                 adjVertice = {actualNode.getPoints().first + 1, actualNode.getPoints().second};
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
             if (actualNode.getPoints().first == 0)
@@ -105,9 +101,9 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
             if (actualNode.getPoints().first == numLines - 1)
@@ -116,9 +112,9 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
 
@@ -128,18 +124,18 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
 
                 adjVertice = {actualNode.getPoints().first, actualNode.getPoints().second + 1};
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
             if (actualNode.getPoints().second == 0)
@@ -148,9 +144,9 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
             if (actualNode.getPoints().second == numColumns - 1)
@@ -159,9 +155,9 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
                 if (!isVisited[adjVertice.first][adjVertice.second] && this->isValidDepth(initialPoints, adjVertice, depth))
                 {
                     isVisited[adjVertice.first][adjVertice.second] = true;
-                    Node node(++nodeCounter, this->map[adjVertice.first][adjVertice.second], adjVertice, &vertexList.at(parentCounter));
+                    float accumulatedWeight = actualNode.getAccumulatedWeight() + this->map[adjVertice.first][adjVertice.second];
+                    Node node(this->map[adjVertice.first][adjVertice.second], adjVertice, actualNode.getPreviousPoints(), accumulatedWeight);
                     frontier.push(node);
-                    vertexList.push_back(node);
                 }
             }
 
@@ -171,37 +167,15 @@ void IDS::getIDS(vector<string> initialPosition, int numLines, int numColumns)
             }
 
             actualNode = frontier.top();
-            parentCounter = actualNode.getVertex();
             frontier.pop();
         }
-        cout << endl;
     }
 
-    Node *aux;
-    for (int i = vertexList.size() - 1; i >= 0; i--)
+    cout << finalNode.getAccumulatedWeight() << " ";
+    for (int i = 1; i < finalNode.getPreviousPoints().size(); i++)
     {
-        if (vertexList.at(i).getPoints().first == finalPoints.first && vertexList.at(i).getPoints().second == finalPoints.second)
-        {
-            aux = &vertexList.at(i);
-            break;
-        }
-    }
-
-    vector<pair<int, int>> responsePoints;
-    float responseWeight = 0.0;
-    while (aux != nullptr)
-    {
-        responsePoints.push_back(aux->getPoints());
-        responseWeight += aux->getWeight();
-        aux = aux->getPreviousNode();
-    }
-    responseWeight -= this->map[initialPoints.first][initialPoints.second];
-
-    cout << responseWeight << " ";
-
-    for (int i = responsePoints.size() - 1; i >= 0; i--)
-    {
-        cout << "(" << responsePoints.at(i).first << "," << responsePoints.at(i).second << ") ";
+        pair<int, int> aux = finalNode.getPreviousPoints().at(i);
+        cout << "(" << aux.first << "," << aux.second << ") ";
     }
     cout << endl;
 }
